f5b8b2bcd53b5ad7021c8a98fe88285aac103806
VishnuVardhan <VishnuVardhan@Rev-PF3C66CF>
2025-09-07 10:19:25 +0530

    project base

---
+++ src/main/java/com/skoler/SkolerApplication.java
@@ -0,0 +1,13 @@
+package com.skoler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkolerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkolerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skoler/SkolerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skoler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkolerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
